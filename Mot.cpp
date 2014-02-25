6150a8672a0730a2bbf11941822d0b76387bea55
Amine Ben hammou <webneat@wn.(none)>
2014-02-25 19:37:11 +0100

    init

--- Mot.cpp
+++ Mot.cpp
@@ -1,33 +1,22 @@
-
 #include "Mot.hpp"
 
 string Mot::getValue(){
 	return value;	
 }
 
-
-
 Mot::Mot(string value , int nbrOccur){
 	this->value = value;
 	this->nbrOccur = nbrOccur;
 }
 
-
 void Mot::setValue(string value){
 	this->value = value;
 }
 
-
 int Mot::getNbrOccur(){
 	return nbrOccur;
-}	
-
+}
 
 void Mot::setNbrOccur(int nbrOccur){
 	this->nbrOccur = nbrOccur;
-}
-
-
-bool Mot::operator<(const Mot & mot){
-	return ( this->nbrOccur < mot.nbrOccur );
 }
